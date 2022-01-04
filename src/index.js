c25474368e4152080a5693b05d03889050e96ea3
Pavel Lebedev <bodyshop.ru@gmail.com>
2022-01-04 17:39:31 +0300

    feat: end function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   if (n > 0) {
+      return n.toString().split('').reverse().join('');
+   } else {
+      return n.toString().split('').reverse().join('').slice(0, -1);
+   }
+
 }
